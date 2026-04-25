4b9b198b2fbd3be2832e77c79593541fe0d60a32
Kakrona Pos <2025007pos@aupp.edu.kh>
2026-04-26 03:06:50 +0700

    change ver.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import numpy as np
 from PIL import Image
 
@@ -9,7 +9,7 @@ LABELS = ["Healthy", "Early Blight", "Late Blight", "Leaf Spot"]
 
 @st.cache_resource
 def load_model(path):
-    interpreter = tf.lite.Interpreter(model_path=path)
+    interpreter = tflite.Interpreter(model_path=path)
     interpreter.allocate_tensors()
     return interpreter
 
